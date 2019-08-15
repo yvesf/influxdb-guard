241e1ab4b4ab12fcce75e098bab94aefd57ac216
marc <mlasch@mailbox.org>
2019-08-15 14:50:05 +0200

    Refactored js arrow function

--- static/main.js
+++ static/main.js
@@ -2,10 +2,10 @@
 // TOKEN_LENGTH is provided by a inline script
 const ALPHABET_BASE58 = '123456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ';
 
-function genToken() {
+const genToken = () => {
   const rand_buffer = Array.from(window.crypto.getRandomValues(new Uint32Array(TOKEN_LENGTH)));
   const rand_chars = rand_buffer.map(elem => ALPHABET_BASE58.charAt(Math.round(elem/4294967295*58)));
   const token = rand_chars.join("");
 
   document.getElementById("tokenField").value = token;
-}
+};
